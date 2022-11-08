baad8de84814367affb1c4008805c2c0894d1bb0
Marica D'souza <maricadsouza221197@gmail.com>
2022-11-08 17:33:28 +0530

    chore: Added footer with github link

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Filters from './components/Filters';
 import { MobileFiltersToggler, MobileFiltersDrawer } from './components/MobileFilters';
 import ProductGrid from './components/ProductGrid';
 import CartProvider from './context/CartContext';
+import { Footer } from './components/Footer';
 
 function App() {
   var today = new Date();
@@ -31,6 +32,8 @@ function App() {
           <ProductGrid></ProductGrid>
         </div>
       </CartProvider>
+
+      <Footer></Footer>
     </div>
     
     
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer className="Footer-container black-text f-medium">
+            <div>© 2022 DevStop</div>
+            <div> ✨ Made by &nbsp;
+                <a href="https://github.com/marination">@marination</a>
+                ✨
+            </div>
+        </footer>
+    );
+}
