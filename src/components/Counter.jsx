802ac23f1a38c350b6fe57f7579eca032b34cde1
Marica D'souza <maricadsouza221197@gmail.com>
2022-12-21 20:19:19 +0100

    chore: Formatting for readability

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,29 @@
 import { useCart } from '../context/CartContext';
 
 const Counter = (props) => {
-    const { cartItems, setCartQty, increaseCartQty, decreaseCartQty } = useCart();
-    let cartCounterQty = cartItems[props.item_id];
+	const { cartItems, setCartQty, increaseCartQty, decreaseCartQty } = useCart();
+	let cartCounterQty = cartItems[props.item_id];
 
-    return (
-        <div className="Counter-container">
-            <div className="Counter">
-                <button onClick={() => increaseCartQty(props.item_id)}>+</button>
-                <input 
-                    type='number' min="0" max="999" 
-                    value={cartCounterQty}
-                    onChange={(e) => setCartQty(props.item_id, e.target.value)}>    
-                </input>
-                <button onClick={() => decreaseCartQty(props.item_id)}>-</button>
-            </div>
-        </div>
-       
-    );
+	return (
+		<div className="Counter-container">
+			<div className="Counter">
+				<button onClick={() => increaseCartQty(props.item_id)}>
+					+
+				</button>
+
+				<input
+					type='number' min="0" max="999"
+					value={cartCounterQty}
+					onChange={(e) => setCartQty(props.item_id, e.target.value)}>
+				</input>
+
+				<button onClick={() => decreaseCartQty(props.item_id)}>
+					-
+				</button>
+			</div>
+		</div>
+
+	);
 }
 
 export default Counter;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,40 +5,36 @@ const ProductCard = (props) => {
   let data = props.item;
 
   return (
-    <div className='Item-container'>
-      <div className='Item-image'>
-        <img className='Item-image-actual' src={data.image} alt={data.name}></img>
+	<div className='Item-container'>
+		<div className='Item-image'>
+			<img className='Item-image-actual' src={data.image} alt={data.name}></img>
 
-        {
-          data.new && <div className="New-label f-small">New Trend</div>
-        }
+			{data.new && <div className="New-label f-small">New Trend</div>}
 
-        {/* Ratings */}
-        <div className='Item-ratings f-small'>
-          <b>{data.ratings}</b> ⭐️  |  <b>{data.reviews}</b>
-        </div>
+			{/* Ratings */}
+			<div className='Item-ratings f-small'>
+				<b>{data.ratings}</b> ⭐️  |  <b>{data.reviews}</b>
+			</div>
 
-        {/* Cart Button */}
-        <div className="overlay">
-          <AddToCartBtn class="Micro-add-to-cart" item_id={props.id}></AddToCartBtn>
-          <AddedtoCartBtn item_id={props.id}></AddedtoCartBtn>
-        </div>
-      </div>
+			{/* Cart Button */}
+			<div className="overlay">
+				<AddToCartBtn class="Micro-add-to-cart" item_id={props.id}></AddToCartBtn>
+				<AddedtoCartBtn item_id={props.id}></AddedtoCartBtn>
+			</div>
+		</div>
 
-      <div className='Item-meta-details'>
-        <h3 className='f-normal pb-5'>{data.brand}</h3>
-        <h4 className='text-muted f-medium'>{data.name}</h4>
+		<div className='Item-meta-details'>
+			<h3 className='f-normal pb-5'>{data.brand}</h3>
+			<h4 className='text-muted f-medium'>{data.name}</h4>
 
-        <div className='Item-price-details'>
-        <p className='m-0'>
-          <span className='f-medium purple-text'><b> {"₹"}&nbsp;{data.price} </b></span>
-          <span className='text-muted f-small'><s>{"₹"}&nbsp;{data.old_price}</s></span>
-        </p>
-      </div>
-
-      </div>
-      
-    </div>
+			<div className='Item-price-details'>
+				<p className='m-0'>
+					<span className='f-medium purple-text'><b> {"₹"}&nbsp;{data.price} </b></span>
+					<span className='text-muted f-small'><s>{"₹"}&nbsp;{data.old_price}</s></span>
+				</p>
+			</div>
+		</div> 
+	</div>
   );
 }
 
