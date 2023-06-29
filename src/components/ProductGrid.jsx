3b2a97bca6b1a61163e0eca181a31f5f494da598
Marica D'souza <maricadsouza221197@gmail.com>
2023-06-29 21:19:19 +0100

    chore: Add more general styles and use map instead of foreach
    - Add reusable styles for font-weight and display flex

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -2,12 +2,12 @@ import ProductCard from "./ProductCard";
 import { ProductData } from "../ProductData";
 
 export default function ProductGrid (props) {
-	let itemList = [];
-	Object.keys(ProductData).forEach((item_id) => {
+	let itemList = Object.keys(ProductData).map((item_id) => {
 		let item_data = ProductData[item_id];
-		itemList.push(
+		return (
 			<ProductCard item={item_data} id={item_id} key={item_id}></ProductCard>
 		);
+
 	});
 
 	return (
